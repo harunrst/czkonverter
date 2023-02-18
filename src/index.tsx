3a58b730a443435dd89d56054be875623f2a1a21
harunrst <harun5hy@outlook.com>
2023-02-18 13:01:57 +0100

    cleanup app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Home from "./views/home";
 import "./index.css";
-import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
---
+++ src/views/home/index.tsx
@@ -0,0 +1,11 @@
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>Homeke</h1>
+    </div>
+  );
+};
+
+export default Home;
